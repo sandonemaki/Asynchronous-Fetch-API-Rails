8afb63e24368a719f4f33adcf4d855320b091c09
sandonemaki <kamuramaki@gmail.com>
2023-02-13 19:33:13 +0900

    http://localhost:3000/get_post で GET も POST も受け付けるルーティングに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get 'photos', to: 'photos#show'
-  post 'photos', to: 'photos#show'
+  get 'get_post', to: 'home#get_post'
+  post 'get_post', to: 'home#get_post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
