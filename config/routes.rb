d5cfd1a036af12ecb05386197a8f812ba67e45ad
sandonemaki <kamuramaki@gmail.com>
2023-02-13 19:03:32 +0900

    最初のコミット

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'photos', to: 'photos#show'
+  post 'photos', to: 'photos#show'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
