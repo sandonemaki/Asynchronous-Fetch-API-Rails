ef0442c4f0262489e5bda043197c34c638f7ac71
sandonemaki <kamuramaki@gmail.com>
2023-02-15 00:47:15 +0900

    特定のエレメントが存在している場合のみ処理を続行

--- app/javascript/entrypoints/js/get.js
+++ app/javascript/entrypoints/js/get.js
@@ -1,40 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   const buttonGet = document.getElementById('button_get_js');
-  buttonGet.addEventListener('click', function(){
-    fetch('/get_post', {
-      method: 'GET'
-    })
-      .then(function(response){
-        const response_message = response.status + ':' + response.statusText
-        console.log(response_message);
-        window.alert('response_message=' + response_message);
-      });
-  });
-
+  if (buttonGet) {
+    buttonGet.addEventListener('click', function(){
+      fetch('/get_post', {
+        method: 'GET'
+      })
+        .then(function(response){
+          const response_message = response.status + ':' + response.statusText
+          console.log(response_message);
+          window.alert('response_message=' + response_message);
+        });
+    });
+  }
   const buttonPost = document.getElementById('button_post_js');
-  buttonPost.addEventListener('click', function(){
-    const post_data = {
-      key1: "data1",
-      key2: "data2",
-      key3: 3
-    };
+  if (buttonPost) {
+    buttonPost.addEventListener('click', function(){
+      const post_data = {
+        key1: "data1",
+        key2: "data2",
+        key3: 3
+      };
 
-    fetch('/get_post', {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': getCsrfToken()
-      },
-      body: JSON.stringify(post_data),
-    })
-      .then(function(response){
-        const response_message = response.status + ':' + response.statusText
-        console.log(response_message);
-        window.alert('response_message=' + response_message);
-      });
-  });
-
+      fetch('/get_post', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': getCsrfToken()
+        },
+        body: JSON.stringify(post_data),
+      })
+        .then(function(response){
+          const response_message = response.status + ':' + response.statusText
+          console.log(response_message);
+          window.alert('response_message=' + response_message);
+        });
+    });
+  }
   const getCsrfToken = () => {
     const metas = document.getElementsByTagName('meta');
     console.log(metas);
